700e651379d6de4beb6f08ca81c3cb2177990e2a
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-04 14:52:49 +0530

    basic component building

--- angular/practice-project/src/app/app.module.ts
+++ angular/practice-project/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AlertComponent} from "./alert/alert.component";
+import {SuccessAlertComponent} from "./alert/successAlert/successAlert.component";
+import {WarningAlertComponent} from "./alert/warningAlert/warningAlert.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
