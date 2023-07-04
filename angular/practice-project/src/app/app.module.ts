49febd3302d72ffcd8caeab26d75e8a6ad12be17
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-04 16:57:25 +0530

    two way data binding basic

--- angular/practice-project/src/app/app.module.ts
+++ angular/practice-project/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {AlertComponent} from "./alert/alert.component";
 import {SuccessAlertComponent} from "./alert/successAlert/successAlert.component";
 import {WarningAlertComponent} from "./alert/warningAlert/warningAlert.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {WarningAlertComponent} from "./alert/warningAlert/warningAlert.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
