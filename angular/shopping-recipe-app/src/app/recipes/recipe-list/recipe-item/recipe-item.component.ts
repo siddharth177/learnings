dae734736e031ce6780c9fac0fd7b19d4126c0aa
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-07 20:59:10 +0530

    child to parent and other components data binding - long way

--- angular/shopping-recipe-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ angular/shopping-recipe-app/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Recipe} from "../../recipe.model";
 
 @Component({
@@ -8,4 +8,8 @@ import {Recipe} from "../../recipe.model";
 })
 export class RecipeItemComponent {
   @Input('recipeItem') recipeItem: Recipe;
+  @Output('recipeSelected') recipeSelected = new EventEmitter<void>();
+  onRecipeSelect() {
+    this.recipeSelected.emit();
+  }
 }
