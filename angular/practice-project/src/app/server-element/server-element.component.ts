ec309a8ccd14aa4efded9b63cb506cf0dac595f0
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-07 13:00:29 +0530

    ViewEncapsulation.None to remove angular attributes on components and make styling global.

--- angular/practice-project/src/app/server-element/server-element.component.ts
+++ angular/practice-project/src/app/server-element/server-element.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
 import {Element} from "../element.model";
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ServerElementComponent {
 
