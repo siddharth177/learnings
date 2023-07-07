1160a7ea8126f51c3d86a9bcc4fb259ab8de9064
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-07 22:47:39 +0530

    Dynamic property chang - @HostListener

--- angular/practice-project/src/app/basicHighlight/better-highlight.directive.ts
+++ angular/practice-project/src/app/basicHighlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -9,8 +9,20 @@ export class BetterHighlightDirective {
   }
 
   ngOnInit() {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement, 'background-color', 'blue'
+    // )
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
     this.renderer.setStyle(
       this.elementRef.nativeElement, 'background-color', 'blue'
+    )
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement, 'background-color', 'transparent'
     )
   }
 }
