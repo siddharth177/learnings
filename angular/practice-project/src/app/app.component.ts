6af7f058662f38863b76ce280b056db1f23a65cd
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-07 18:01:01 +0530

    refractory - cleaned app component code

--- angular/practice-project/src/app/app.component.ts
+++ angular/practice-project/src/app/app.component.ts
@@ -8,21 +8,4 @@ import {Element} from "./element.model";
 })
 export class AppComponent {
   title = 'practice-project';
-
-  serverElements: Element[] = [];
-
-  onAddServer(serverElement: {name: string, content: string}) {
-    this.serverElements.push(new Element('server', serverElement.name, serverElement.content));
-  }
-
-  onAddBlueprint(blueprintElement: {name: string, content: string}) {
-    this.serverElements.push(new Element('blueprint', blueprintElement.name, blueprintElement.content));
-  }
-
-  onServerBlueprintLastRemove(isReset: {removeLast: boolean}) {
-    if(isReset) {
-      this.serverElements.splice(this.serverElements.length - 1, 1);
-    }
-  }
-
 }
