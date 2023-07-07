18c231d4a22443d2a072531b016ede7706742773
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-07 19:21:34 +0530

    game control project completed

--- angular/practice-project/src/app/app.component.ts
+++ angular/practice-project/src/app/app.component.ts
@@ -7,5 +7,22 @@ import {Element} from "./element.model";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'practice-project';
+  title = 'game-control-practice-project';
+  // @ts-ignore
+  currentNumber: number;
+  // @ts-ignore
+  allNumbers: number[] = [];
+
+  getCurrentNumber(newNumber: number) {
+    this.currentNumber = newNumber;
+  }
+
+  onPause(isPause: boolean) {
+    if (isPause)
+      this.allNumbers.push(this.currentNumber);
+  }
+
+  onClear() {
+    this.allNumbers = [];
+  }
 }
--- angular/practice-project/src/app/game-control/game-control.component.ts
+++ angular/practice-project/src/app/game-control/game-control.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {TimeInterval} from "rxjs";
 
 @Component({
   selector: 'app-game-control',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class GameControlComponent {
 
+  isPause = false;
+  interval: any;
+  // @ts-ignore
+  @Output('newNumber') newNumber = new EventEmitter<number>();
+  @Output('isPaused') isPaused = new EventEmitter<boolean>();
+  @Output('isCleared') isCleared = new EventEmitter<boolean>();
+
+  onPlay() {
+    if (!this.isPause) {
+      this.interval = setInterval(() => {
+        this.newNumber.emit(Math.floor(Math.random() * 1000) + 1)
+      }, 1000);
+    } else {
+      this.isPause = false;
+      this.onPlay();
+    }
+  }
+
+  onPause() {
+    this.isPause = true;
+    this.isPaused.emit(true);
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  onClear() {
+    this.isCleared.emit(true);
+  }
 }
