663fd40d39b7ed534af521d33380a077bdce253a
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-08 12:45:31 +0530

    logging service created (using object creation - not the right way)

--- angular/practice-project/src/app/account/account.component.ts
+++ angular/practice-project/src/app/account/account.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {LoggingService} from "../services/logging.service";
 
 @Component({
   selector: 'app-account',
@@ -16,6 +17,7 @@ export class AccountComponent {
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    const loggingService = new LoggingService();
+    loggingService.logStatusChange(status);
   }
 }
