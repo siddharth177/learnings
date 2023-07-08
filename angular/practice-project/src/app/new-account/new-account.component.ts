e86b600dcfb00c59a7e202f5633b6e864f1149ac
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-08 12:49:22 +0530

    Hierarchical Injector - Injecting Logging Service using providers

--- angular/practice-project/src/app/new-account/new-account.component.ts
+++ angular/practice-project/src/app/new-account/new-account.component.ts
@@ -1,11 +1,16 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {LoggingService} from "../services/logging.service";
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
+
+  constructor(private loggingService: LoggingService) {
+  }
 
   @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
@@ -14,6 +19,6 @@ export class NewAccountComponent {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
