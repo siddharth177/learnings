8626730bd4dd222be67234cf958809ba95b4cb01
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-08 21:09:34 +0530

    Passing parameter to route

--- angular/practice-project/src/app/app.module.ts
+++ angular/practice-project/src/app/app.module.ts
@@ -31,6 +31,7 @@ import {RouterModule, Routes} from "@angular/router";
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'users/:id', component: UserComponent},
   {path: 'servers', component: ServersComponent},
 ]
 @NgModule({
