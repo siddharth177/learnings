50f056aa0e3021d298a3c8cbff5f5843c0e78bf8
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-08 18:16:07 +0530

    adding ingredients to recipe list

--- angular/shopping-recipe-app/src/app/services/recipe.service.ts
+++ angular/shopping-recipe-app/src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from "../recipes/recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,11 @@ import {Recipe} from "../recipes/recipe.model";
 export class RecipeService {
 
   private _recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'For Testing', 'https://www.foodandwine.com/thmb/FUO2HaAAAPkIKrAfyrE6G8ls-H0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/recipetester-ft-0318-2710aa8b7070463f89de25a168261b05.jpg')
+    new Recipe('Test Recipe', 'For Testing',
+      'https://www.foodandwine.com/thmb/FUO2HaAAAPkIKrAfyrE6G8ls-H0=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/recipetester-ft-0318-2710aa8b7070463f89de25a168261b05.jpg'),
+    new Recipe('Sample Recipe', 'For Sample',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272',
+      [new Ingredient('meat', 2), new Ingredient('chilli', 1)])
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
