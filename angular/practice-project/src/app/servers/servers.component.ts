a208a8f71b2ec2ac3454b69e20a6eca80eea92b1
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-08 21:07:42 +0530

    navigating programmatically - setting relative path using ActivatedRoute

--- angular/practice-project/src/app/servers/servers.component.ts
+++ angular/practice-project/src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,18 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onServersReload() {
+    this.router.navigate(['servers']);  // will work - navigate doesn't know the current path
+
+    // this.router.navigate(['servers'], {relativeTo: this.activatedRoute}); // sets the relative path - won't work as we don't have servers/servers path
   }
 
 }
