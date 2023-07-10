62b57c1ed3144b0fdfae476fca37763d4929b06c
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-10 13:55:18 +0530

    header navigation links

--- angular/shopping-recipe-app/src/app/app-routing.module.ts
+++ angular/shopping-recipe-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {RecipesComponent} from "./recipes/recipes.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: "full"},
+  {path: 'shopping-list', component: ShoppingListComponent},
+  {path: 'recipes', component: RecipesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
