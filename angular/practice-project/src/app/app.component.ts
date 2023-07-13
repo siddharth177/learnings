b3d9cb8d7a85b10f3028df4313b293a1529f3ef2
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-13 15:15:19 +0530

    Template driven form approach practice

--- angular/practice-project/src/app/app.component.ts
+++ angular/practice-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,12 @@ import {Component} from '@angular/core';
 export class AppComponent {
   // @ts-ignore
   title: 'handling-forms-practice-project';
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  selectedSubscription = 'Basic';
+  // @ts-ignore
+  @ViewChild('signupForm') signupForm: NgForm;
+
+  onsubmit() {
+
+  }
 }
