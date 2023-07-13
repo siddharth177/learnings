676fcf383ce5c1961c63c6f84f16f19921bec910
siddharth <siddharthkatiyar170701@gmail.com>
2023-07-13 12:36:11 +0530

    refactored for practice on TD approach for forms

--- angular/practice-project/src/app/app.component.ts
+++ angular/practice-project/src/app/app.component.ts
@@ -1,9 +1,4 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {Element} from "./element.model";
-import {AccountsService} from "./services/accounts.service";
-import {UserService} from "./user/user.service";
-import {Subscription} from "rxjs";
-import {NgForm} from "@angular/forms";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,53 +6,7 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css'],
   providers: []
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   // @ts-ignore
   title: 'handling-forms-practice-project';
-  defaultQuestion = 'pet';
-  answer = '';
-  genders = ['male', 'female'];
-  formSubmitted: boolean = false;
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  };
-
-  // @ts-ignore
-  @ViewChild('f') signupForm: NgForm;
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    this.signupForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    })
-  }
-
-  onSubmit() {
-    console.log(this.signupForm);
-    this.formSubmitted = true;
-
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.secret;
-    this.user.answer = this.signupForm.value.questionAnswer;
-    this.user.gender = this.signupForm.value.gender;
-
-    this.signupForm.resetForm();
-  }
-
-  ngOnDestroy() {
-  }
-
 }
