ed1964d36768e69a5361f75e0b4f3ed345f08737
Siddharth <siddharthkatiyar170701@gmail.com>
2023-12-24 23:26:06 +0530

    models, List, map

--- flutter_projects/quiz_app/lib/questions.dart
+++ flutter_projects/quiz_app/lib/questions.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +14,22 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screen');
+    var currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ...currentQuestion.answers.map((e) => AnswerButton(e, () {}))
+        ],
+      ),
+    );
   }
 }
