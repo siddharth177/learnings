b69cfa23553e716a09c57241bedbb965bf93bf3b
Siddharth <siddharthkatiyar170701@gmail.com>
2023-12-24 18:35:30 +0530

    creating variables

--- flutter_projects/lib/gradient_container.dart
+++ flutter_projects/lib/gradient_container.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_projects/text_widget.dart';
 
+var startAlignment = Alignment.topRight;
+var endAlignment = Alignment.topLeft;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Colors.yellow, Colors.red],
-          begin: Alignment.topRight,
-          end: Alignment.topLeft,
+          colors: const [Colors.yellow, Colors.red],
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(child: TextWidget()),
