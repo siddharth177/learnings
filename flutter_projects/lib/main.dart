b5e0d4ecd7fb45d8591285b0030e6610a4236551
Siddharth <siddharthkatiyar170701@gmail.com>
2023-12-24 18:27:20 +0530

    Widget Class
    Outsourcing custom widget logic to separate class

--- flutter_projects/lib/main.dart
+++ flutter_projects/lib/main.dart
@@ -3,19 +3,25 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       // runApp is provided by Flutter
-      home: Scaffold(
-          body: Container(
-    decoration: const BoxDecoration(
-      gradient: LinearGradient(
-        colors: [Colors.yellow, Colors.red],
-        begin: Alignment.topRight,
-        end: Alignment.topLeft,
+      home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.yellow, Colors.red],
+          begin: Alignment.topRight,
+          end: Alignment.topLeft,
+        ),
       ),
-    ),
-    child: const Center(
-        child: Text(
-      'Hello World!',
-      style: TextStyle(color: Colors.white),
-    )),
-  ))));
+      child: const Center(
+          child: Text(
+        'Hello World!',
+        style: TextStyle(color: Colors.white),
+      )),
+    );
+  }
 }
