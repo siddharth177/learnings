943de9c064633416da4f95ea5e55c67b0d3b7a37
Siddharth <siddharthkatiyar170701@gmail.com>
2023-12-24 17:47:01 +0530

    runApp

--- flutter_projects/lib/main.dart
+++ flutter_projects/lib/main.dart
@@ -1,1 +1,7 @@
- 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      // runApp is provided by Flutter
+      home: Scaffold(body: Center(child: Text('Hello World')))));
+}
