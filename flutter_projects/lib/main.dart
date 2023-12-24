5aeceb4f737abcd208aa0117f505e7d32d19d5e4
Siddharth <siddharthkatiyar170701@gmail.com>
2023-12-24 18:29:32 +0530

    class constructor

--- flutter_projects/lib/main.dart
+++ flutter_projects/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
       // runApp is provided by Flutter
       home: Scaffold(body: GradientContainer())));
 }
 
 class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
