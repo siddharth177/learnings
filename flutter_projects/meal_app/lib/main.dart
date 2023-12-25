931883d99face93c8a257ed2c0f8c51c7acafb80
Siddharth <siddharthkatiyar170701@gmail.com>
2023-12-25 10:36:40 +0530

    RunApp added

--- flutter_projects/meal_app/lib/main.dart
+++ flutter_projects/meal_app/lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const App());
+}
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: theme,
+        home:
+    );
+  }
+
+}
