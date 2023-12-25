c26f58cd84fd1220ee1b22f99ad66bd1ef53b7c6
Siddharth <siddharthkatiyar170701@gmail.com>
2023-12-25 10:43:58 +0530

    Grid View Prepared

--- flutter_projects/meal_app/lib/main.dart
+++ flutter_projects/meal_app/lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'categories.dart';
+
 void main() {
   runApp(const App());
 }
-
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: theme,
-        home:
-    );
+    return MaterialApp(theme: ThemeData.dark(), home: CategoriesScreen());
   }
-
 }
